906bf180cfe5986923950d09de94d5f020ab5a0f
Michael Constantin <constantinm@sharplabs.com>
2017-01-07 22:47:55 -0800

    added brushes

--- mod3/tkinter/canvas/paint2.py
+++ mod3/tkinter/canvas/paint2.py
@@ -8,9 +8,20 @@ python_green = "#476042"
 
 
 class Brush:
-    def __init__(self, width=1, color='black'):
+    def __init__(self, name, width=1, color='black'):
+        self.__name = name
         self.__width = width
         self.__color = color
+        self.selected = False
+        self.__id = None
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def id(self):
+        return self.__id
 
     @property
     def width(self):
@@ -24,7 +35,16 @@ class Brush:
         """
         Draw a circular brush on the canvas, centered on (x,y)
         """
-        canvas.create_oval(x-self.width/2, y-self.width/2, x+self.width/2, y+self.width/2, fill=self.color)
+        self.__id = canvas.create_oval(x - self.width / 2, y - self.width / 2, x + self.width / 2, y + self.width / 2,
+                                fill=self.color, tags=('brushes', self.name))
+
+    def __str__(self):
+        return 'brush name={}, width={}, color={}'.format(self.name, self.width, self.color)
+
+    def __eq__(self, other):
+        if not isinstance(other, Brush):
+            raise TypeError('other is not a Brush')
+        return self.width == other.width and self.color == other.color
 
 
 class BrushesPanel:
@@ -37,6 +57,7 @@ class BrushesPanel:
         self.y1 = int(y1)
         self.canvas = canvas
         self.brushes = list()
+        self.default = None
 
     def add_brushes(self, *widths):
         """
@@ -51,12 +72,16 @@ class BrushesPanel:
 
         panel_width = self.x1 - self.x0
         panel_height = self.y1 - self.y0
+        count = len(widths)
 
         if max(widths) + 2 * self.spacer > panel_height:
             raise ValueError("panel height cannot fit the brushes")
         if sum(widths) + self.spacer * (len(widths) + 1) > panel_width:
             raise ValueError("panel width cannot fit the brushes")
-        self.brushes = [Brush(width) for width in widths]
+
+        self.brushes = [Brush('b' + str(i), width) for i, width in zip(range(1, count + 1), widths)]
+        self.brushes.sort(key=lambda x: x.width, reverse=True)
+        self.default = self.brushes[count//2]
 
     def draw(self):
         panel_width = self.x1 - self.x0
@@ -70,6 +95,31 @@ class BrushesPanel:
 
         for x, y, b in zip(x_pos, y_pos, self.brushes):
             b.draw(self.x0 + x, self.y0 + y, self.canvas)
+            self.canvas.tag_bind(b.name, '<Button-1>',
+                                 lambda event, panel=self, brush=b: BrushesPanel.select(panel, brush, event))
+
+    def get_brush(self, id):
+        return list(filter(lambda b: b.id == id, self.brushes))[0]
+
+    def get_selected(self):
+        for b in self.brushes:
+            if b.selected:
+                return b
+        else:
+            return self.default
+
+    def _deselect(self):
+        for b in self.brushes:
+            b.selected = False
+
+    def select(self, brush, event):
+        canvas = event.widget
+        brush_id = canvas.find_closest(event.x, event.y)[0]
+        closest_brush = self.get_brush(brush_id)
+        assert brush == closest_brush
+        self._deselect()
+        brush.selected = True
+        return brush
 
 
 def paint(event):
@@ -80,9 +130,15 @@ def paint(event):
         return
 
     x2, y2 = event.x, event.y
-    w.create_line(x1, y1, x2, y2, fill=python_green)
+    # find selected brush
+    brush = panel.get_selected()
+    w.create_line(x1, y1, x2, y2, width=brush.width, fill=brush.color, smooth=True, capstyle=ROUND, joinstyle=ROUND)
     x1, y1 = x2, y2
 
+
+def clear(event):
+    global x1, y1
+    x1 = y1 = None
 
 master = Tk()
 master.title("Painting using Lines")
@@ -96,6 +152,7 @@ panel.add_brushes(15, 10, 5, 3, 1)
 panel.draw()
 
 w.bind("<B1-Motion>", paint)
+w.bind("<ButtonRelease>", clear)
 
 message = Label(master, text="Press and Drag the mouse to draw")
 message.pack(side=BOTTOM)
